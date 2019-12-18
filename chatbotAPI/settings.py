19dd970502a2a508557a8075765c675df2642963
95twan <95twan@naver.com>
2019-12-18 19:58:39 +0900

    setting circle CI3

--- chatbotAPI/settings.py
+++ chatbotAPI/settings.py
@@ -15,14 +15,16 @@ import json
 
 from django.core.exceptions import ImproperlyConfigured
 
+secrets = ""
+
 if os.path.isfile("secrets.json"):
     with open("secrets.json") as f:
         secrets = json.loads(f.read())
 
 
-def get_secret(setting, secrets=secrets):
+def get_secret(setting, secret=secrets):
     try:
-        return secrets[setting]
+        return secret[setting]
     except KeyError:
         error_msg = "Set the {0} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
