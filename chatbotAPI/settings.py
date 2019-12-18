41ace31847261a5152c3e64ec1e4141dee89b280
95twan <95twan@naver.com>
2019-12-18 20:02:18 +0900

    setting circle CI5

--- chatbotAPI/settings.py
+++ chatbotAPI/settings.py
@@ -15,17 +15,17 @@ import json
 
 from django.core.exceptions import ImproperlyConfigured
 
-if os.path.isfile("secrets.json"):
-    with open("secrets.json") as f:
-        secrets = json.loads(f.read())
-
-
-def get_secret(setting, secret=secrets):
-    try:
-        return secret[setting]
-    except KeyError:
-        error_msg = "Set the {0} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+# if os.path.isfile("secrets.json"):
+#     with open("secrets.json") as f:
+#         secrets = json.loads(f.read())
+#
+#
+# def get_secret(setting, secret=secrets):
+#     try:
+#         return secret[setting]
+#     except KeyError:
+#         error_msg = "Set the {0} environment variable".format(setting)
+#         raise ImproperlyConfigured(error_msg)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
