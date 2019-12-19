8057bb699250bf8db1ed7217501cd1697013a0cc
95twan <95twan@naver.com>
2019-12-19 21:52:35 +0900

    setting circle CI18

--- chatbotAPI/settings.py
+++ chatbotAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['54.180.81.200', '127.0.0.1', 'localhost']
 
 
 # Application definition
